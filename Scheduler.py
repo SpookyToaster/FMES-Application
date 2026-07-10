0f91c646e2c558137a24695a17542d3904759776
LoganBurkardt <lb1031545@otc.edu>
2026-07-10 12:35:14 -0500

    Initial commit

---
+++ Scheduler.py
@@ -0,0 +1,34 @@
+# // =======================================================
+# // Scheduling Module - Prep and Mold
+# // =======================================================
+
+# // Read input from file for list of ready to schedule jobs
+# File 
+
+
+# // Manage list of jobs by catergories so like-alloys can be scheduled together
+
+# // check list of requirements for each day and add new next job line if it will not exceed requirements
+
+# // if job exceeds single day requirements for itself then 
+
+# // if exceeding requirements schedule on the following weekday
+
+# // export when complete
+
+# // =======================================================
+# // Scheduling Module - Melt
+# // =======================================================
+
+# // If job has been scheduled and mold shows complete, but no heat number assigned. Show ready to pour
+
+# // If ready to pour, read into list
+
+# // for item in list add to ready-to-pour schedule
+
+# // =======================================================
+# // Cleaning Schedule
+# // =======================================================
+
+# If heat number assigned, but not shipped, show on cleaning schedule sorted by due date.
+
